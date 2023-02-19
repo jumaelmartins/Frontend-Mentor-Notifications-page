570368a48410ad0d6f12562abf444ba805edb1b5
jumaelmartins <jumaelmartins@gmail.com>
2023-02-19 17:00:31 -0300

    Função que remove a class unread ao clicar na notificação

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,32 @@ console.log(noticationsCounter, markAsRead, unreadNotifications);
 const cleanNotifications = () => {
   markAsRead.addEventListener("click", (e) => {
     noticationsCounter.innerHTML = 0;
+    removeUnreadClass();
+  });
+};
+
+const removeUnreadClass = () => {
+  unreadNotifications.forEach((notification) => {
+    notification.classList.remove("unread");
+  });
+};
+
+const removeUnreadClassByClick = () => {
+  unreadNotifications.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      const notification = item.closest("li");
+
+      if (notification.classList.contains("unread")) {
+        notification.classList.remove("unread");
+      }
+      return;
+    });
   });
 };
 
 const init = () => {
   cleanNotifications();
+  removeUnreadClassByClick();
 };
 
 init();
