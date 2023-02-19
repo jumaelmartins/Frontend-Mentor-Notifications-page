2808282d396d1348b607d5c37d32f36b15cc72f5
jumaelmartins <jumaelmartins@gmail.com>
2023-02-19 17:21:54 -0300

    Criado função para decrementar o contador de notificação ao clicar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,6 @@ const noticationsCounter = document.querySelector("#notications");
 const markAsRead = document.querySelector("#mark-as-read");
 const unreadNotifications = document.querySelectorAll("li");
 
-console.log(noticationsCounter, markAsRead, unreadNotifications);
-
 const cleanNotifications = () => {
   markAsRead.addEventListener("click", (e) => {
     noticationsCounter.innerHTML = 0;
@@ -22,12 +20,24 @@ const removeUnreadClassByClick = () => {
     item.addEventListener("click", (e) => {
       const notification = item.closest("li");
 
+      decrementCounterByClickNotification();
+
       if (notification.classList.contains("unread")) {
         notification.classList.remove("unread");
       }
       return;
     });
   });
+};
+
+const decrementCounterByClickNotification = () => {
+  const notificationUnreadCounter = Array.from(unreadNotifications).filter(
+    (item) => {
+      return item.classList.contains("unread");
+    }
+  );
+
+  noticationsCounter.innerHTML = notificationUnreadCounter.length - 1;
 };
 
 const init = () => {
